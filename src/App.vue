b396824b1921879740329884075c0256a238bd30
Willian Barreto Froes <willian.froes_BOLS@pampa.compasso>
2019-09-10 17:24:30 -0300

    [FEAT] upload project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="corpo">
+    
+  </div>
+</template>
+
+<script>
+import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+
+export default {
+  
+}
+</script>
+
+<style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/shared/cadastro/Cadastro.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2 class="centralizado"></h2>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .centralizado {
+        text-align: center;
+    }
+</style>
